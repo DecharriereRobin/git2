c65d78d48bbc89ef1d4a6d77d179761853875e74
doncodie <don.codie@hotmail.fr>
2016-11-12 17:58:56 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<ul>
+		<li>Menu 1</li>
+		<li>Menu 2</li>
+		<li>Menu 3</li>
+	</ul>
+</body>
+</html>
